bee54d319c98b0bc231f990ba94d860e0903f620
yashb007 <bansaly37@gmail.com>
2021-10-17 16:14:02 +0530

    Updates

---
+++ precache-manifest.308cb08a3e68a5bdf657fca9d10542c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6022d2a6bbf173d95f40",
+    "url": "/SocialMedia-using-Blockchain/static/css/main.811c4d72.chunk.css"
+  },
+  {
+    "revision": "6022d2a6bbf173d95f40",
+    "url": "/SocialMedia-using-Blockchain/static/js/main.6022d2a6.chunk.js"
+  },
+  {
+    "revision": "1d2c949393935d0ccba5",
+    "url": "/SocialMedia-using-Blockchain/static/css/1.bb1f27de.chunk.css"
+  },
+  {
+    "revision": "1d2c949393935d0ccba5",
+    "url": "/SocialMedia-using-Blockchain/static/js/1.1d2c9493.chunk.js"
+  },
+  {
+    "revision": "1cd8b00c5694593c2fb1",
+    "url": "/SocialMedia-using-Blockchain/static/js/runtime~main.1cd8b00c.js"
+  },
+  {
+    "revision": "03e2effbf7d52b9fc25e7447973455bf",
+    "url": "/SocialMedia-using-Blockchain/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/SocialMedia-using-Blockchain/precache-manifest.308cb08a3e68a5bdf657fca9d10542c0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/SocialMedia-using-Blockchain/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
